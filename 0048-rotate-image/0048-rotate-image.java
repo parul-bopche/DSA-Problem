7549a56362334989126bbe4d5e54dca39b84035e
Parul Bopche <parulbopche0420@gmail.com>
2025-08-23 22:32:15 +0530

    Time: 0 ms (100%), Space: 42 MB (89.7%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        //pehle transpose krr matrix ko 
+        //n*n he to matrix ki length ko direct n manle
+        int n = matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //abb tu reverse krr row wise
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
