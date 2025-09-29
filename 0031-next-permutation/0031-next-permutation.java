969c2e9e962317b35a15459443392977d43a1883
Parul Bopche <parulbopche0420@gmail.com>
2025-09-29 10:46:39 +0530

    Time: 0 ms (100%), Space: 43 MB (76.1%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+
+        // Step 1: Find the break-point
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            // Last permutation → just reverse to get first
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Step 2: Find the next greater element from right
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums, index + 1, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
